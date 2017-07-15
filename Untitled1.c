1b1389445f41ef4256c421321415bc37c6d95ebb
Manikandaprabu22 <mkprabu4007@gmail.com>
2017-07-15 20:34:57 +0530

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;//s=0,r=0,n=0,c=0;
+//clrscr();
+for(i=0;i<9999;i++)
+{
+   /* while(n!=0){
+s=i%10;
+r=r+s;
+n=i/10;
+if(r==7){
+printf("-->%d >",i);
+}}*/printf("->%d",i);
+}
+getch();
+}
